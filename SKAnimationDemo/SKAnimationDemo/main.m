d61ec3f141924d58c5c8ef77ccf7f997b668a0f0
AlexanderYeah <yeshengke@163.com>
2018-01-25 11:20:19 +0800

    init

---
+++ SKAnimationDemo/SKAnimationDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SKAnimationDemo
+//
+//  Created by AY on 2018/1/25.
+//  Copyright © 2018年 AY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
